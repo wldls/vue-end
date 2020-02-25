31c0667d053fea9ead0a0d03b5cfae7e1e6d4a80
jiin <dslet2343@gmail.com>
2020-02-25 11:41:02 +0900

    ..

--- vue-til/src/components/SignupForm.spec.js
+++ vue-til/src/components/SignupForm.spec.js
@@ -1,21 +1,21 @@
 import { shallowMount } from '@vue/test-utils';
-import { SignupForm } from '@/components/SignupForm.vue';
+import SignupForm from '@/components/SignupForm.vue';
 
 describe('SignupForm.vue', () => {
-  test('메일주소가 유효한지 확인', () => {
+  test('메일주소가 유효하지 않으면 경고 문구가 생성된다.', () => {
     const wrapper = shallowMount(SignupForm, {
       data() {
         return {
-          username: '',
+          username: 'test',
           password: '',
         };
       },
     });
 
     const txtError = wrapper.find('.txt-err');
-    // const button = wrapper.find('button');
+    const button = wrapper.find('button');
 
     expect(txtError.exists()).toBeTruthy();
-    // expect(button.element.disabled).toBeTruthy();
+    expect(button.element.disabled).toBeTruthy();
   });
 });
